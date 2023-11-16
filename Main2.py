fe4561ce6844e39864dd050c0a6519f82e4174ba
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-11-16 13:44:19 -0800

    added fading old screen and old  music #22 #23

--- Main2.py
+++ Main2.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import pygame
 from screens.LoadingScreen import LoadingScreen
 from screens.TestScreen import TestScreen
@@ -7,24 +8,39 @@ from screens.TestScreen import TestScreen
 pygame.init()
 
 # Set up screen dimensions
-screen_width, screen_height = 800, 600
+screen_width, screen_height = 1500, 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Brown Zelda (But Not Garbage)")
 
+pygame.mixer.music.load(os.path.join("Assets", "originalzeldatitlemusic.mp3"))  
+pygame.mixer.music.set_volume(0.5)
+
 def init_home_screen():
     current_screen = LoadingScreen(screen)
+    loadingscreenstarttime = time.time()
 
+    pygame.mixer.music.play(-1)
+    
     gameLoop = True
+    loadingScreenDone = False
+    testScreenStarted = False
     while gameLoop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                gameLoop = False
+            elif loadingScreenDone and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                 x, y = event.pos
-                if 0 <= x <= 800 and 0 <= y <= 600:
+                if 0 <= x <= 1500 and 0 <= y <= 800:
                     current_screen = TestScreen(screen)
+                    testScreenStarted = True
 
-        current_screen.display()
+        if not loadingScreenDone:
+            elapsedTime = time.time() - loadingscreenstarttime
+            if elapsedTime > 5:
+                loadingScreenDone = True
+            current_screen.display(elapsedTime)
+        if testScreenStarted:
+            current_screen.display()
         pygame.display.update()
 
     pygame.quit()
--- screens/LoadingScreen.py
+++ screens/LoadingScreen.py
@@ -1,22 +1,30 @@
 import pygame
 import sys
+import os
+import time
 from .Screen import Screen 
 
 class LoadingScreen(Screen):
     def __init__(self, screen):
         super().__init__(screen)
-        font = pygame.font.Font(None, 36)
-        self.text = font.render("Loading...", True, (0, 0, 0))
-        self.text_rect = self.text.get_rect(center=(self.screen_width // 2, self.screen_height // 2))
 
-    def display(self):
+        self.originaltitlescreenimg = pygame.image.load(os.path.join("Assets", "originalzeldatitlescreen.jpeg"))
+        self.newtitlescreenimg = pygame.image.load(os.path.join("Assets", "newzeldatitlescreen.png"))
+
+        self.originaltitlescreenimg = pygame.transform.scale(self.originaltitlescreenimg, (self.screen_height, self.screen_width))
+        self.newtitlescreenimg = pygame.transform.scale(self.newtitlescreenimg, (self.screen_height, self.screen_width))
+
+        self.transitiontime = 5
+    
+    def display(self, elapsedTime):
+        percentage_complete = (elapsedTime / self.transitiontime) * 100
+
+        current_image = pygame.Surface(self.originaltitlescreenimg.get_size())
+        current_image.set_alpha(int(255 - percentage_complete))
+        current_image.blit(self.originaltitlescreenimg, (0, 0))
+
         self.screen.fill((255, 255, 255))
-        self.screen.blit(self.text, self.text_rect)
+        self.screen.blit(current_image, (0, 0))
 
-    def handle_events(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            return "test"
-
-        return None
     
     
