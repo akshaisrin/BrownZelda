d76bf6ed345dc68efde94cfe1ed293e4fdf71910
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-11-28 19:03:22 -0800

    fixed up elapsed time thing instructions #41

--- Main2.py
+++ Main2.py
@@ -14,6 +14,7 @@ from inputs import get_gamepad
 from XBoxController import *
 from Player import *
 from items.Sword import Sword
+from Obstacle import *
 
 pygame.init()
 
@@ -107,12 +108,12 @@ def init_instructions_screen():
         if elapsedTime > 72:
             pygame.mixer.music.stop()
             gameLoop = False
-        elif elapsedTime > 70:
-            current_screen.displayfade(elapsedTime - 76)
-        elif elapsedTime > 66:
+        elif elapsedTime > 68:
+            current_screen.displayfade(elapsedTime - 68)
+        elif elapsedTime > 65:
             pygame.mixer.music.set_volume(.1)
         else:    
-            current_screen.display()
+            current_screen.display(elapsedTime)
         pygame.display.update()
 
 def init_home_screen():
@@ -120,8 +121,9 @@ def init_home_screen():
     #monster1=TestMonster(10.0, 9.0, "Test Monster 1", 800, 100, 250, 300)
     monster2=TestMonsterMedium(10.0, 9.0, "Test Monster 2", 500, 100, 250, 300)
 
-
-    player1 = Player("bheem", {}, "", 1, 1.2, 5,5,5, "str", 500, 500, 0)
+    player1 = Player("bheem", {}, "", 1, 1.2, 3,5,5, "str", 500, 500, 0)
+   
+    sword = Sword()
 
     sword = Sword()
 
@@ -129,7 +131,6 @@ def init_home_screen():
         joystick=XboxController()
     except:
         controller_detected=False
-
     # Establishing game loop to keep screen running
 
     gameLoop = True
@@ -138,53 +139,64 @@ def init_home_screen():
     pressed_right=False
     pressed_up=False
     pressed_down=False
-    
+
     while gameLoop:
         
         screen.fill((255,255,255))
-        healthBarDisplay = font.render('Player Health: ' + str(player1.health_bar), True, Color(0, 0, 0))
-        screen.blit(healthBarDisplay, (1200, 100))
-        #monster1.render(800, 100, 250, 300, screen)
-        #monster1.shoot(screen)
+        # pygame.draw.rect(screen, (255, 0, 255), player1.player_rectangle)
+        health_bar_display = font.render('Player Health: ' + str(player1.health_bar), True, Color(0, 0, 0))
+        screen.blit(health_bar_display, (1200, 100))
+
+        lives_display = font.render('Lives Remaining: ' + str(player1.lives_remaining), True, Color(0, 0, 0))
+        screen.blit(lives_display, (1200, 150))
+        # monster1.render(800, 100, 250, 300, screen)        
+        # monster1.shoot(screen)
+        
         if monster2.alive:
             monster2.render(500, 100, 250, 300, screen)
-        # monster2.shoot(screen, player1)
+            monster2.shoot(screen, player1)
+            if (player1.player_rectangle.colliderect(monster2.projectile.projectile_rectangle)):
+                print("player got hit")
+                monster2.realign_projectile()
+                player1.get_attacked(monster2.projectile.damage, screen)
+            
+            if sword.attacking:
+                elaspedTime = time.time() - attacktime
+                if elaspedTime > 0.5:
+                    sword.attacking = False
+                elif elaspedTime > 0.25:
+                    sword.render(player1.player_rectangle.x + 75 + (100 * elaspedTime), player1.player_rectangle.y + 75 + (100 * elaspedTime), 50, 50, screen)
+                else:
+                    sword.render(player1.player_rectangle.x + 100 - (100 * elaspedTime), player1.player_rectangle.y + 100 - (100 * elaspedTime), 50, 50, screen)
 
-        player1.render(player1.x_pos,player1.y_pos, 300, 300, screen)
-        if sword.attacking:
-            elaspedTime = time.time() - attacktime
-            if elaspedTime > 0.5:
-                sword.attacking = False
-            elif elaspedTime > 0.25:
-                sword.render(player1.x_pos + 75 + (100 * elaspedTime), player1.y_pos + 75 + (100 * elaspedTime), 50, 50, screen)      
-            else:
-                sword.render(player1.x_pos + 100 - (100 * elaspedTime), player1.y_pos + 100 - (100 * elaspedTime), 50, 50, screen)      
-
+            # obj1 = Obstacle("washington.jpeg", 0, 0,"irrelevant")
+            # obj1.render(200, 200, screen)  
+        player1.render(player1.player_rectangle.x,player1.player_rectangle.y, 300, 300, screen)
         if controller_detected:
             new_state=(joystick.get_x_axis(), joystick.get_y_axis())
-            
+
             # player movement with x box controller
 
-            if (new_state[0]<-1*Constants.controller_threshold):
-                print("move left")
-                player1.direction = "left"
-                player1.move()
-                
-            if (new_state[0]>Constants.controller_threshold):
-                print("move right")
-                player1.direction = "right"
-                player1.move()
-
-            if (new_state[1]<-1*Constants.controller_threshold):
-                print("move down")
-                player1.direction = "down"
-                player1.move()
+        if (new_state[0]<-1*Constants.controller_threshold):
+            print("move left")
+            player1.direction = "left"
+            player1.move()
             
-            if (new_state[1]>controller_threshold):
-                print("move up")
-                player1.direction = "up"
-                player1.move()   
-     
+        if (new_state[0]>Constants.controller_threshold):
+            print("move right")
+            player1.direction = "right"
+            player1.move()
+        
+        if (new_state[1]<-1*Constants.controller_threshold):
+            print("move down")
+            player1.direction = "down"
+            player1.move()
+
+        if (new_state[1]>controller_threshold):
+            print("move up")
+            player1.direction = "up"
+            player1.move()   
+
         for event in pygame.event.get():
 
             if event.type == pygame.KEYDOWN:                   
@@ -197,20 +209,20 @@ def init_home_screen():
                 elif event.key == pygame.K_DOWN:     
                     pressed_down = True
 
-            elif event.type == pygame.KEYUP:            
-                if event.key == pygame.K_LEFT:        
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
                     pressed_left = False
-                elif event.key == pygame.K_RIGHT:     
+                elif event.key == pygame.K_RIGHT:
                     pressed_right = False
-                elif event.key == pygame.K_UP:       
+                elif event.key == pygame.K_UP: 
                     pressed_up = False
                 elif event.key == pygame.K_DOWN:
-                    pressed_down = False
+                     pressed_down = False
                 elif (event.key == pygame.K_SPACE) and not sword.attacking:
-                    print("attack")
-                    attacktime = time.time()
-                    sword.attack(player1, monster2)
-
+                     print("attack")
+                     attacktime = time.time()
+                     sword.attack(monster2)
+            
             if event.type == pygame.QUIT:
                 gameLoop=False
                 pygame.quit()
@@ -219,18 +231,19 @@ def init_home_screen():
         if pressed_left:
             player1.direction = "left"
             player1.move() 
-        
+
         if pressed_right:
             player1.direction = "right"
             player1.move() 
+
         if pressed_up:
             player1.direction = "up"
-            player1.move() 
-            
+            player1.move()
+        
         if pressed_down:
             player1.direction = "down"
             player1.move() 
-
+        
         pygame.display.update()
 
 
--- screens/InstructionsScreen.py
+++ screens/InstructionsScreen.py
@@ -9,21 +9,25 @@ class InstructionsScreen(Screen):
         super().__init__(screen)
 
         self.img = pygame.image.load(os.path.join("Assets", "prologuescreen.png"))
+        print(self.img.get_height())
         self.img = pygame.transform.scale(self.img, (self.screen_width, int(self.img.get_height())))
+        
 
         self.img_x = 0
         self.img_y = 0
         
-        self.scroll_speed = -0.3
+        self.scrolltime = 66
+        self.screen_height = -2064
         self.fadetime = 1.5
     
-    def display(self):
-        self.img_y += self.scroll_speed
+    def display(self, elapsedTime):
+        percentage_complete = elapsedTime / self.scrolltime
+        self.img_y = self.screen_height * percentage_complete
 
         self.screen.blit(self.img, (self.img_x, self.img_y))
         
     def displayfade(self, elapsedTime):
-        percentage_complete = ((elapsedTime + 2) / self.fadetime) * 100
+        percentage_complete = (elapsedTime  / self.fadetime) * 100
 
         current_image = pygame.Surface(self.img.get_size())
         current_image.set_alpha(int(255 - percentage_complete))
