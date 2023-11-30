6547a75f6df450fc7e24bc5be41268d94a2ac1cb
arjunbalaji99 <arjunbalaji08@gmail.com>
2023-11-30 11:38:33 -0800

    added sprite walking #43

--- Main2.py
+++ Main2.py
@@ -12,7 +12,7 @@ from TestMonster import *
 from TestMonsterMedium import *
 from inputs import get_gamepad
 from XBoxController import *
-from Player import *
+from Player2 import *
 from items.Sword import Sword
 from Obstacle import *
 
@@ -33,11 +33,6 @@ def init_loading_screen():
     pygame.mixer.music.set_volume(0.6)
     pygame.mixer.music.play()
     
-    # first, start default music and loading screen
-    # then, have it start fading to black, and cut the music
-    # then, pause for a second, and start the chotta bheem music
-    # then, quickly have it come to the new screen
-
     gameLoop = True
     initialLoadingScreenDone = False
     newLoadingScreenDone = False
@@ -119,136 +114,45 @@ def init_instructions_screen():
         pygame.display.update()
 
 def init_home_screen():
-    controller_detected=True
-    #monster1=TestMonster(10.0, 9.0, "Test Monster 1", 800, 100, 250, 300)
-    monster2=TestMonsterMedium(10.0, 9.0, "Test Monster 2", 500, 100, 250, 300)
-
-    player1 = Player("bheem", {}, "", 1, 1.2, 3,5,5, "str", 500, 500, 0)
-   
-    sword = Sword()
-
-    sword = Sword()
-
-    try:
-        joystick=XboxController()
-    except:
-        controller_detected=False
-    # Establishing game loop to keep screen running
-
+    clock = pygame.time.Clock() 
+    player1 = Player2("bheem", {}, "", 1, 1.2, 3,5,5, "str", 500, 500, 0)
+    
     gameLoop = True
-    attacktime = None
-    pressed_left=False
-    pressed_right=False
-    pressed_up=False
-    pressed_down=False
-
+    direction = None
+    framecounter = 0
     while gameLoop:
-        
+        clock.tick(60)
+        framecounter = framecounter + 1
         screen.fill((255,255,255))
-        # pygame.draw.rect(screen, (255, 0, 255), player1.player_rectangle)
-        health_bar_display = font.render('Player Health: ' + str(player1.health_bar), True, Color(0, 0, 0))
-        screen.blit(health_bar_display, (1200, 100))
-
-        lives_display = font.render('Lives Remaining: ' + str(player1.lives_remaining), True, Color(0, 0, 0))
-        screen.blit(lives_display, (1200, 150))
-        # monster1.render(800, 100, 250, 300, screen)        
-        # monster1.shoot(screen)
-        
-        if monster2.alive:
-            monster2.render(500, 100, 250, 300, screen)
-            monster2.shoot(screen, player1)
-            if (player1.player_rectangle.colliderect(monster2.projectile.projectile_rectangle)):
-                print("player got hit")
-                monster2.realign_projectile()
-                player1.get_attacked(monster2.projectile.damage, screen)
-            
-            if sword.attacking:
-                elaspedTime = time.time() - attacktime
-                if elaspedTime > 0.5:
-                    sword.attacking = False
-                elif elaspedTime > 0.25:
-                    sword.render(player1.player_rectangle.x + 75 + (100 * elaspedTime), player1.player_rectangle.y + 75 + (100 * elaspedTime), 50, 50, screen)
-                else:
-                    sword.render(player1.player_rectangle.x + 100 - (100 * elaspedTime), player1.player_rectangle.y + 100 - (100 * elaspedTime), 50, 50, screen)
-
-            # obj1 = Obstacle("washington.jpeg", 0, 0,"irrelevant")
-            # obj1.render(200, 200, screen)  
-        player1.render(player1.player_rectangle.x,player1.player_rectangle.y, 300, 300, screen)
-        if controller_detected:
-            new_state=(joystick.get_x_axis(), joystick.get_y_axis())
-
-            # player movement with x box controller
-
-        if (new_state[0]<-1*Constants.controller_threshold):
-            print("move left")
-            player1.direction = "left"
-            player1.move()
-            
-        if (new_state[0]>Constants.controller_threshold):
-            print("move right")
-            player1.direction = "right"
-            player1.move()
-        
-        if (new_state[1]<-1*Constants.controller_threshold):
-            print("move down")
-            player1.direction = "down"
-            player1.move()
-
-        if (new_state[1]>controller_threshold):
-            print("move up")
-            player1.direction = "up"
-            player1.move()   
 
         for event in pygame.event.get():
-
             if event.type == pygame.KEYDOWN:                   
-                if event.key == pygame.K_LEFT:        
-                    pressed_left = True
-                elif event.key == pygame.K_RIGHT:     
-                    pressed_right = True
-                elif event.key == pygame.K_UP:        
-                    pressed_up = True
-                elif event.key == pygame.K_DOWN:     
-                    pressed_down = True
-
+                if event.key == pygame.K_LEFT: direction = "left"
+                elif event.key == pygame.K_RIGHT: direction = "right"
+                elif event.key == pygame.K_UP: direction = "up"
+                elif event.key == pygame.K_DOWN: direction = "down"
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    pressed_left = False
-                elif event.key == pygame.K_RIGHT:
-                    pressed_right = False
-                elif event.key == pygame.K_UP: 
-                    pressed_up = False
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    player1.current_frame = 10
+                    direction = None
+                elif event.key == pygame.K_UP:
+                    player1.current_frame = 11
+                    direction = None
                 elif event.key == pygame.K_DOWN:
-                     pressed_down = False
-                elif (event.key == pygame.K_SPACE) and not sword.attacking:
-                     print("attack")
-                     attacktime = time.time()
-                     sword.attack(monster2)
+                    player1.current_frame = 9
+                    direction = None
             
             if event.type == pygame.QUIT:
                 gameLoop=False
                 pygame.quit()
                 sys.exit()
 
-        if pressed_left:
-            player1.direction = "left"
-            player1.move() 
+        player1.handlemove(direction, framecounter)
+        player1.render(player1.player_rectangle.x, player1.player_rectangle.y, screen)
 
-        if pressed_right:
-            player1.direction = "right"
-            player1.move() 
-
-        if pressed_up:
-            player1.direction = "up"
-            player1.move()
-        
-        if pressed_down:
-            player1.direction = "down"
-            player1.move() 
-        
         pygame.display.update()
 
 
-init_loading_screen()
-init_instructions_screen()
+# init_loading_screen()
+# init_instructions_screen()
 init_home_screen()
---
+++ Player2.py
@@ -0,0 +1,129 @@
+import pygame
+import Constants
+import os
+import sys
+import time
+
+class Player2:
+
+    def __init__(self, username: str, inventory:dict, curr_item:str, curr_level:int, curr_checkpoint:tuple, lives_remaining: int, health_bar: int, wealth: int, img:str, start_x, start_y, start_z):
+        self.spritesheet=pygame.image.load(os.path.join("Assets", "chotta-bheem-spritesheet.png"))
+        self.spritesheet=pygame.transform.scale(self.spritesheet, (576, 192))
+        self.current_frame = 9
+        self.spritesheet_frames = [self.spritesheet.subsurface((i * 64, j * 64, 64, 64)) for j in range(3) for i in range(9)]
+        self.player_rectangle=self.spritesheet_frames[self.current_frame].get_rect()
+        self.player_rectangle.topleft = (start_x, start_y)
+        self.flipped = False
+        self.lastup = None
+        self.lastleft = None
+        self.direction = None
+
+        self.username = username
+        self.inventory = inventory
+        self.curr_item = curr_item
+        self.curr_level = curr_level
+        self.curr_checkpoint = curr_checkpoint
+        self.lives_remaining = lives_remaining
+        self.original_health=health_bar
+        self.health_bar = self.original_health
+        self.wealth = wealth
+
+    def select_item(self, item:str):
+        if item in self.inventory:
+            self.curr_item = item
+
+    def use_item(self):
+        self.curr_item.use() #use function will be defined in Item classes
+
+    def get_attacked(self, damage:int, screen):
+        self.health_bar -= damage
+
+        if (self.health_bar<=0):
+            print("player just lost all their health")
+            self.lives_remaining-=1
+            self.die_and_begone(screen)
+
+
+    def get_healed(self, healing:int):
+        self.health_bar += healing
+    
+    
+    def die_and_begone(self, screen, overworld=None):
+        if self.lives_remaining <= 0:
+            print("GAME OVER")
+            game_over_img=pygame.image.load(os.path.join("Assets", "game_over_screen.jpg"))
+            game_over_img = pygame.transform.scale(game_over_img, (Constants.screen_width, Constants.screen_height))
+            screen.blit(game_over_img, (0, 0))
+
+            pygame.display.update()
+            time.sleep(3)
+            sys.exit()
+            #overworld.game_over()
+        else:
+            self.respawn() 
+
+
+    def respawn(self):
+        self.health_bar=self.original_health
+        print("PLAYER RESPAWNED")
+
+        # self.x_pos = self.curr_checkpoint[0]
+        # self.y_pos = self.curr_checkpoint[1]
+        # self.z_pos = self.curr_checkpoint[2]
+
+    def handlemove(self, direction, framecounter): 
+        self.direction = direction
+        if self.direction == None:
+            return
+        elif self.direction == "left":
+            if framecounter % 2 == 0:
+                if self.current_frame != 10:
+                    self.current_frame = 10
+                else:
+                    if self.lastleft == 13:
+                        self.current_frame = 16
+                        self.lastleft = 16
+                    else:
+                        self.current_frame = 13
+                        self.lastleft = 13
+                if self.flipped:
+                    self.spritesheet_frames = [pygame.transform.flip(frame, True, False) for frame in self.spritesheet_frames]
+                    self.flipped = False
+        elif self.direction == "right":
+            if framecounter % 2 == 0:
+                if self.current_frame != 10:
+                    self.current_frame = 10
+                else:
+                    if self.lastleft == 13:
+                        self.current_frame = 16
+                        self.lastleft = 16
+                    else:
+                        self.current_frame = 13
+                        self.lastleft = 13
+                if not self.flipped:
+                    self.spritesheet_frames = [pygame.transform.flip(frame, True, False) for frame in self.spritesheet_frames]
+                    self.flipped = True
+        elif self.direction == "up":
+            if framecounter % 2 == 0:
+                if self.lastup == 14:
+                    self.current_frame = 17
+                    self.lastup = 17
+                else:
+                    self.current_frame = 14
+                    self.lastup = 14
+        else:
+            if framecounter % 2 == 0:
+                if self.lastup == 12:
+                    self.current_frame = 15
+                    self.lastup = 15
+                else:
+                    self.current_frame = 12
+                    self.lastup = 12
+        
+        self.player_rectangle.move_ip(Constants.directions[self.direction][0], Constants.directions[self.direction][1])
+        
+    def render(self, x_pos:float, y_pos:float,screen:pygame.display) -> None:
+        image = pygame.transform.scale(self.spritesheet_frames[self.current_frame], (64, 64))
+        self.player_rectangle = image.get_rect()
+        self.player_rectangle.topleft = (x_pos, y_pos)
+        screen.blit(image, self.player_rectangle)
