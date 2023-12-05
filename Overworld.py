e685fa4652e435df688ba24f56d198f6fe6482a4
= <=>
2023-12-05 12:25:00 -0800

    restarting work

--- Overworld.py
+++ Overworld.py
@@ -40,13 +40,8 @@ class Overworld(Room):
         self.homes = Biome("homes", "homes_biome.png", 1200, 500, True, 500, 400)
         self.tundra = Biome("tundra", "tundra_biome.png", 1200, 500, True, 500, 400)
         self.zelda = Biome("zelda", "zelda_biome.png", 1200, 500, True, 500, 400)
-<<<<<<< HEAD
         self.dungeon = Biome("dungeon", "dungeon1_copy.jpg", -1000, -1000, False)
-        self.test_room = Biome("test_room", "background.jpg", 650, 600, False)
-=======
-        self.dungeon = Biome("dungeon", "dungeons1.jpg", -1000, -1000, False)
         self.test_room = Biome("test_room", "background.jpg", 640, 600, False)
->>>>>>> abd76ff (merging dungeons and biomes)
         self.test_room2 = Biome("test_room", "test_room.png", 1200, 500, True, 290, 0)
                 
         # add obstacles to each Biome
@@ -113,7 +108,7 @@ class Overworld(Room):
         #print(player.player_rectangle.topleft)
         #print(curr_biome.exit_x)
         #print(curr_biome.exit_y)
-        if (player.player_rectangle.topleft[0] < curr_biome.exit_x + 40 and player.player_rectangle.topleft[0] > curr_biome.exit_x - 40) and (player.player_rectangle.topleft[1] < curr_biome.exit_y + 40 and player.player_rectangle.topleft[1] > curr_biome.exit_y - 40):
+        if (player.player_rectangle.topleft[0] < curr_biome.exit_x + 10 and player.player_rectangle.topleft[0] > curr_biome.exit_x - 10) and (player.player_rectangle.topleft[1] < curr_biome.exit_y + 10 and player.player_rectangle.topleft[1] > curr_biome.exit_y - 10):
             x_pos = screen_height
             while x_pos > 0:
                 curr_screen_x_pos -= 100
